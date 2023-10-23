d2a7cc86ce6d3e69dbfd2f1096bef33ed4f6f2f4
Saksham  Chaudhary <122385363+sakshamchaudhary123@users.noreply.github.com>
2023-10-23 04:56:59 -0400

    Update armstrong.java

--- armstrong.java
+++ armstrong.java
@@ -1,5 +1,26 @@
 public class armstrong {
     public static void main(String[] args) {
+         int n,c,arm=0,rem;
+        System.out.print("Enter the number=");
+        Scanner r=new Scanner(System.in);
+        n=r.nextInt();
+        c=n;
+
+        while(n>0)
+        {
+            rem=n%10;
+            arm=(rem*rem*rem)+arm;
+            n=n/10;
+        }
+        if(c==arm)
+        {
+          System.out.println("Armstrong Number");
+        }
+        else
+        {
+            System.out.println("Not Armstrong Number");
+        }
+        r.close();
     
     }
 }
